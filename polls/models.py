be0183024169bfb36ba125c0b2a7c1bae115d22b
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 11:15:53 +0700

    create 2 new models: question and choise

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# the question has the question text and the pubblication date
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+# the choise has the text and number of the time the associated question has been chosen 
+class Choise(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
