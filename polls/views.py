7aae03d66f2f3c971846d283d0e81d1f2cee7f05
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 12:43:53 +0700

    add views for questions

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at the question %s" % question_id)
+
+def results(request, question_id):
+    reponse = "your are looking at the results of question %s"
+    return HttpResponse(reponse % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting the question %s" % question_id)
