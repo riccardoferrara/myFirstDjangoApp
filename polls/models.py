454652390f80f86160476955db740efe5e97b5c4
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 11:46:54 +0700

    introduce new method for recently published questions

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,14 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # the question has the question text and the pubblication date
 class Question(models.Model):
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
