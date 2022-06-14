6f0d890fa073defccb14dc4d1b1f07ca069b003e
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 11:43:36 +0700

    add str to class

--- polls/models.py
+++ polls/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # the question has the question text and the pubblication date
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
 # the choise has the text and number of the time the associated question has been chosen 
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
