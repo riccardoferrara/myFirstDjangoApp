7e5244ca1d1916dd985231e397639f45da852aff
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 15:54:49 +0700

    using "render" is faster

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+
 
 from .models import Question
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    # load the template
-    template = loader.get_template('polls/index.html')
-    # create variable to pass to the template
     context = {'latest_question_list': latest_question_list}
     # give response rendering the template
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse("You are looking at the question %s" % question_id)
