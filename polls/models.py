76a65d5a394e08f11a0f9aecabccc3bd522d340e
riscaldo <riccardo85ferrara@gmail.com>
2022-06-14 11:43:05 +0700

    correct name

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
 # the choise has the text and number of the time the associated question has been chosen 
-class Choise(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
